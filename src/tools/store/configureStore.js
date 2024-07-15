ee4d8a07da98f48cb4cb45b7afee1b7a2a4f0cad
Lala Mammadzada <160578995+LalaMammadzada24@users.noreply.github.com>
2024-07-15 12:57:07 +0400

    Delete src directory

--- src/tools/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import {createStore} from 'redux'
-import { blogReducer } from '../reducer/blogReducer';
-
-const configureStore = () => {
-    const store = createStore(blogReducer);
-    return store
-}
-
-export default configureStore;
